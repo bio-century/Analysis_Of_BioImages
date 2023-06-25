0a1410e5eae16a1b915b1fe201ea8af9d8c1c0e9
bio-century <info@bio-century.net>
2023-06-25 15:10:21 +0200

    ellipse fit, comments and print output revision

--- ModulesOwn/A_Groundwork.py
+++ ModulesOwn/A_Groundwork.py
@@ -1,5 +1,5 @@
 # Color DNA with respect to their bases
-def identifyCellContoursAndAreaOverlays(myImage, myImageThresholded, dictThresholdValues):
+def identifyCellContoursAndAreaOverlays(myImage, myImageThresholded, dictThresholdValues, meanFilledArea):
 	# sources
 	# https://www.kaggle.com/code/voglinio/separating-nuclei-masks-using-convexity-defects
 	# https://de.mathworks.com/matlabcentral/answers/43435-i-couldn-t-understand-convex-area
@@ -7,20 +7,26 @@ def identifyCellContoursAndAreaOverlays(myImage, myImageThresholded, dictThresho
 	# https://stackoverflow.com/questions/50591442/convert-3-dim-image-array-to-2-dim
 	# https://www.tutorialspoint.com/how-to-compute-the-area-and-perimeter-of-an-image-contour-using-opencv-python
 
+	# source of main algorithm
+	# https://www.kaggle.com/code/voglinio/separating-nuclei-masks-using-convexity-defects
+	# User: Costas Voglis
+
+	# https://de.mathworks.com/matlabcentral/answers/43435-i-couldn-t-understand-convex-area
+	# prop.convex_area / prop.filled_area
+	# convex_area (convex hull): the smallest region that satisfy two conditions: (1) it is convex (2) it contains
+	# the original region.
+	# => The ratio increases the less convex the area is (multiple nuclei)
+
 	# import modules
 	import cv2
 	import numpy as np
 	import matplotlib.pyplot as plt
 	from skimage.measure import regionprops
 	from statistics import mean
-	import math
 
 	cellCount           = 0
 	contours, hierarchy = cv2.findContours(myImageThresholded.astype(np.uint8), 1, 2)
 	myMaskContoursAll   = np.zeros(myImage.shape)
-	numberOfObjects     = len(contours)
-	list_multicore      = []
-	oneNucleiArea      	= []
 
 	for ii, cnt in enumerate(contours):
 		M               = cv2.moments(cnt)
@@ -39,42 +45,55 @@ def identifyCellContoursAndAreaOverlays(myImage, myImageThresholded, dictThresho
 
 		myMaskZeros       = np.zeros(myImage.shape, np.uint8)
 		myMaskContoursTmp = cv2.drawContours(image = myMaskZeros, contours=[cnt], contourIdx = -1, color = (255, 255, 255), thickness = cv2.FILLED)
-
 		myMaskContours                           = np.zeros(myImage.shape)
 		myMaskContours[myMaskContoursTmp == 255] = 1
-
 		props = regionprops(myMaskContoursTmp, cache = False)
 		prop = props[0]
 		
 		ratio_conv_filled = prop.convex_area / prop.filled_area
-		# https://www.kaggle.com/code/voglinio/separating-nuclei-masks-using-convexity-defects
-		# prop.convex_area / prop.filled_area
-		# convex_area (convex hull): the smallest region that satisfy two conditions: (1) it is convex (2) it contains the original region.
-		# => The ratio increases the less convex the area is (multiple nuclei)
 		# celladd = math.ceil(prop.filled_area)/(2291.9454545454546)
 		# celladd = math.ceil((prop.filled_area)/(2276.51))
-		celladd = round((prop.filled_area)/(2276.51))
-		# print(celladd)
+		celladd = round((prop.filled_area)/(meanFilledArea))
 
-		if  ratio_conv_filled > dictThresholdValues["3CellCluster"]:
+		if  ratio_conv_filled > dictThresholdValues["gt2CellCluster"]:
 			myMaskContoursAll  = myMaskContoursAll + myMaskContours * 0.9
-			list_multicore.append(1)
-			# celladd = 3
 			cellCount=cellCount + celladd
 		elif  ratio_conv_filled > dictThresholdValues["2CellCluster"]:
 			myMaskContoursAll  = myMaskContoursAll + myMaskContours * 0.5
-			list_multicore.append(1)
-			# celladd = 2
 			cellCount = cellCount + celladd
 		else:
 			myMaskContoursAll  = myMaskContoursAll + myMaskContours * 0.2
-			list_multicore.append(0)
-			# celladd = 1
 			cellCount = cellCount + celladd
-			oneNucleiArea.append(prop.filled_area)
 		myMaskContoursAll = cv2.drawContours(myMaskContoursAll, [cnt], -1, (0,0,255), 2)
 		cv2.putText(myMaskContoursAll, f'{ii + 1}', (x1, y1 + 5), cv2.FONT_HERSHEY_COMPLEX, 0.6, (150, 150, 150), 1)
 		cv2.putText(myMaskContoursAll, "%.3f" % ratio_conv_filled, (x1, y1+20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 0, 0), 1)
-	list_avg = mean(oneNucleiArea) 
-	# print(list_avg)
 	return cellCount, contours, myImageContours, myMaskContoursAll
+
+# Color DNA with respect to their bases
+def meanFilledArea(myImage, myImageThresholded, dictThresholdValues):
+	# import modules
+	import cv2
+	import numpy as np
+	import matplotlib.pyplot as plt
+	from skimage.measure import regionprops
+	from statistics import mean
+
+	contours, hierarchy = cv2.findContours(myImageThresholded.astype(np.uint8), 1, 2)
+	oneNucleiArea      	= []
+
+	for ii, cnt in enumerate(contours):
+		perimeter       = cv2.arcLength(cnt, True)
+		perimeter       = round(perimeter, 1)
+		myMaskZeros       = np.zeros(myImage.shape, np.uint8)
+		myMaskContoursTmp = cv2.drawContours(image = myMaskZeros, contours=[cnt], contourIdx = -1, color = (255, 255, 255), thickness = cv2.FILLED)
+		props = regionprops(myMaskContoursTmp, cache = False)
+		prop = props[0]
+		ratio_conv_filled = prop.convex_area / prop.filled_area
+		if  ratio_conv_filled > dictThresholdValues["gt2CellCluster"]:
+			pass
+		elif  ratio_conv_filled > dictThresholdValues["2CellCluster"]:
+			pass
+		else:
+			oneNucleiArea.append(prop.filled_area)
+	meanFilledArea = mean(oneNucleiArea) 
+	return meanFilledArea
